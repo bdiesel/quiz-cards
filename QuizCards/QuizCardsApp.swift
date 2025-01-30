43fde76ae13425a88288863ad64ee9aa0913aefa
Brian Diesel <bri@ndiesel.net>
2025-01-30 14:38:16 -0500

    Initial Commit

---
+++ QuizCards/QuizCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizCardsApp.swift
+//  QuizCards
+//
+//  Created by Brian Diesel on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct QuizCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
