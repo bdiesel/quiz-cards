8e91f7351e3a2e8b31271dc4b3524b8722b12c69
Brian Diesel <bri@ndiesel.net>
2025-01-31 09:55:39 -0500

    addd card stack

--- QuizCards/ContentView.swift
+++ QuizCards/ContentView.swift
@@ -7,9 +7,30 @@
 
 import SwiftUI
 
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
+    }
+}
+
 struct ContentView: View {
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
+    
     var body: some View {
-        CardView(card: .example)
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: self.cards[index])
+                            .stacked(at: index, in: self.cards.count)
+                    }
+                }
+            }
+        }
     }
 }
 
