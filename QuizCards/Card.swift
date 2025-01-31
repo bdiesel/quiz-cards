6529407a0456ed7ff3ffd72d9d709145735803f9
Brian Diesel <bri@ndiesel.net>
2025-01-31 09:44:32 -0500

    Adding single card with answer

---
+++ QuizCards/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  QuizCards
+//
+//  Created by Brian Diesel on 1/31/25.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
