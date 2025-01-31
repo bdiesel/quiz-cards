d4170dc7536c4612ba86e72c810021f8a7de985c
Brian Diesel <bri@ndiesel.net>
2025-01-31 12:40:41 -0500

    change card color on swipe

--- QuizCards/CardView.swift
+++ QuizCards/CardView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+    
     @State private var offset = CGSize.zero
     @State private var isShowingAnswer = false
     
@@ -15,10 +17,21 @@ struct CardView: View {
     var removal: (() -> Void)? = nil
     
     var body: some View {
-            ZStack {
-                RoundedRectangle(cornerRadius: 25)
-                    .fill(.white)
-                    .shadow(radius: 10)
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .fill(
+                    accessibilityDifferentiateWithoutColor
+                    ? .white
+                    : .white
+                        .opacity(1 - Double(abs(offset.width / 50)))
+                )
+                .background(
+                    accessibilityDifferentiateWithoutColor
+                    ? nil
+                    : RoundedRectangle(cornerRadius: 25)
+                        .fill(offset.width > 0 ? .green : .red)
+                )
+                .shadow(radius: 10)
                 VStack {
                     Text(card.prompt)
                         .font(.largeTitle)
